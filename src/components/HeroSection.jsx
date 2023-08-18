fea9f5663265461254678570ecad7defde7b312c
ecostabal <ecostabal@urbex.cl>
2023-08-18 14:35:12 -0400

    mascambios

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,10 +6,9 @@ const links = [
 
   ]
   const stats = [
-    { name: 'Offices worldwide', value: '12' },
-    { name: 'Full-time colleagues', value: '300+' },
-    { name: 'Hours per week', value: '40' },
-    { name: 'Paid time off', value: 'Unlimited' },
+    { name: 'installation', value: 'Super Easy' },
+    { name: 'ready tu use', value: 'Navigation Paths' },
+    { name: 'integration', value: 'Components' },
   ]
   
   export default function HeroSection() {
