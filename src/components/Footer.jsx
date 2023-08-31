dcb80ac8db9a6db80c004817a57a263bbcb97358
ecostabal <ecostabal@urbex.cl>
2023-08-30 23:18:18 -0400

    news

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-slate-100	">
       <div className="container mx-auto justify-center max-w-7xl px-2 sm:px-6 lg:px-8 flex flex-col p-6 space-y-4 sm:space-y-0 sm:flex-row">
-          <p className="text-center	text-sm text-gray-600 dark:text-gray-300">© Copyright 2023. All Rights Reserved - With ❤️ by <b>pucho.dev</b></p>
+          <p className="text-center	text-sm text-gray-600 dark:text-gray-300">© Copyright 2023 - With ❤️ by <b>pucho.dev</b></p>
       </div>
     </footer>   
   );
