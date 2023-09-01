de5f5bc298d736ff3f7599f47fbf01a8781531c8
ecostabal <ecostabal@urbex.cl>
2023-08-31 23:18:44 -0400

    server3

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import path from 'path'; // Importación de path
-import { fileURLToPath } from 'url'; // Agrega esta línea
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url)); // Agrega esta línea
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // Configuración de opciones CORS
 const allowedOrigins = ['http://localhost:5173', 'https://malaga.pucho.dev', 'https://malaga-propiedades.vercel.app'];
@@ -28,21 +27,21 @@ const corsOptions = {
   optionsSuccessStatus: 204
 };
 
-// Servir archivos estáticos
-app.use(express.static('dist'));
-
 // Usa el middleware CORS con las opciones especificadas
 app.use(cors(corsOptions));
+
+// Servir archivos estáticos
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+// Manejo de rutas fallback
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 // Log detallado para depuración
 app.use((err, req, res, next) => {
   console.error('Internal error:', err.stack);
   res.status(500).send('Internal Server Error');
-});
-
-// Manejo de rutas fallback
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5173;
